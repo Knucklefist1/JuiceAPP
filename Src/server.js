f87ddbbb8de4e06c782f9f0aa400361c20bcd58b
Valdemar Marloth A. Andersen <vaan23ab@student.cbs.dk>
2024-11-07 12:15:43 +0100

    Update server.js

--- Src/server.js
+++ Src/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'Src', 'Public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Src', 'Public', 'JuiceApp.html'));
+    res.sendFile(path.join(__dirname, 'Public', 'JuiceApp.html'));
 });
 
 // Start the server on the specified port, or default to 3000
